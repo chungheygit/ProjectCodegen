aab41497a931d6baf3d28bbc99de9545fb5f6ae3
salahb10 <47417425+salahb10@users.noreply.github.com>
2021-05-14 16:55:52 +0200

    AccountService af

--- src/main/java/io/swagger/service/AccountService.java
+++ src/main/java/io/swagger/service/AccountService.java
@@ -1,4 +1,28 @@
 package io.swagger.service;
 
+import io.swagger.model.Account;
+import io.swagger.repository.AccountRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AccountService {
+
+    AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+    public List<Account> GetAllAccounts(){
+        return accountRepository.findAll();
+    }
+    public void deleteAccount(Account account){
+        accountRepository.delete(account);
+    }
+    public void addAccount(Account account){
+         accountRepository.save(account);
+    }
+
 }
+
