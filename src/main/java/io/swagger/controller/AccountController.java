d0c5d24bd8d12838b17e0aaea60dd9bc0975df3b
salahb10 <47417425+salahb10@users.noreply.github.com>
2021-05-14 16:54:33 +0200

    AccountController af

--- src/main/java/io/swagger/controller/AccountController.java
+++ src/main/java/io/swagger/controller/AccountController.java
@@ -1,4 +1,25 @@
 package io.swagger.controller;
 
+import io.swagger.model.Account;
+import io.swagger.service.AccountService;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
 public class AccountController {
+    private AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+    public List<Account> GetAllAccounts() {
+        return accountService.GetAllAccounts();
+    }
+    public void deleteAccount(Account account){
+       accountService.deleteAccount(account);
+    }
+    public void addAccount(Account account){
+        accountService.addAccount(account);
+    }
 }
