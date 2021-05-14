b1d97028f0596defc715d2889c5850ae408ebe9c
salahb10 <47417425+salahb10@users.noreply.github.com>
2021-05-14 16:55:12 +0200

    AccountRepository af

--- src/main/java/io/swagger/repository/AccountRepository.java
+++ src/main/java/io/swagger/repository/AccountRepository.java
@@ -1,4 +1,11 @@
 package io.swagger.repository;
 
-public class AccountRepository {
+import io.swagger.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account,Long> {
+
 }
+
