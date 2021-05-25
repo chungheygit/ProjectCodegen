0e7d2346a6b9c7fdf8cbe7a3115850037bb041a3
LionelSumter <50021812+LionelSumter@users.noreply.github.com>
2021-05-25 18:19:20 +0200

    Add files via upload
    @Entity en @Id toegevoegd

--- src/main/java/io/swagger/model/User.java
+++ src/main/java/io/swagger/model/User.java
@@ -1,26 +1,31 @@
 package io.swagger.model;
 
-import java.util.Objects;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import org.threeten.bp.LocalDate;
 import org.springframework.validation.annotation.Validated;
+import org.threeten.bp.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.validation.Valid;
-import javax.validation.constraints.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.Objects;
 
 /**
  * User
  */
+@Entity
 @Validated
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-05-17T13:44:26.622Z[GMT]")
 
 
-public class User   {
+public class User {
+  @Id
   @JsonProperty("id")
   private Long id = null;
 
@@ -38,6 +43,36 @@ public class User   {
 
   @JsonProperty("password")
   private String password = null;
+
+  @JsonProperty("userType")
+  @Valid
+  private UserTypeEnum userTypeEnum;
+
+  @JsonProperty("dayLimit")
+  private BigDecimal dayLimit = null;
+
+  @JsonProperty("transactionLimit")
+  private BigDecimal transactionLimit = null;
+
+  @JsonProperty("open")
+  private Boolean open = null;
+
+  public User(Long id, String firstName, String lastName, LocalDate dateOfBirth, String email, String password, UserTypeEnum userTypeEnum, BigDecimal dayLimit, BigDecimal transactionLimit, Boolean open) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dateOfBirth = dateOfBirth;
+    this.email = email;
+    this.password = password;
+    this.userTypeEnum = userTypeEnum;
+    this.dayLimit = dayLimit;
+    this.transactionLimit = transactionLimit;
+    this.open = open;
+  }
+
+  //default constructor
+  public User() {
+  }
 
   /**
    * Gets or Sets userType
@@ -69,18 +104,8 @@ public class User   {
       return null;
     }
   }
-  @JsonProperty("userType")
-  @Valid
-  private List<UserTypeEnum> userType = new ArrayList<UserTypeEnum>();
 
-  @JsonProperty("dayLimit")
-  private BigDecimal dayLimit = null;
 
-  @JsonProperty("transactionLimit")
-  private BigDecimal transactionLimit = null;
-
-  @JsonProperty("open")
-  private Boolean open = null;
 
   public User id(Long id) {
     this.id = id;
@@ -203,13 +228,13 @@ public class User   {
     this.password = password;
   }
 
-  public User userType(List<UserTypeEnum> userType) {
-    this.userType = userType;
+  public User userType(UserTypeEnum userType) {
+    this.userTypeEnum = userType;
     return this;
   }
 
   public User addUserTypeItem(UserTypeEnum userTypeItem) {
-    this.userType.add(userTypeItem);
+    this.userTypeEnum = userTypeItem;
     return this;
   }
 
@@ -220,12 +245,12 @@ public class User   {
   @Schema(required = true, description = "")
       @NotNull
 
-    public List<UserTypeEnum> getUserType() {
-    return userType;
+    public UserTypeEnum getUserType() {
+    return userTypeEnum;
   }
 
-  public void setUserType(List<UserTypeEnum> userType) {
-    this.userType = userType;
+  public void setUserType(UserTypeEnum userType) {
+    this.userTypeEnum = userType;
   }
 
   public User dayLimit(BigDecimal dayLimit) {
@@ -294,7 +319,7 @@ public class User   {
 
 
   @Override
-  public boolean equals(java.lang.Object o) {
+  public boolean equals(Object o) {
     if (this == o) {
       return true;
     }
@@ -308,7 +333,7 @@ public class User   {
         Objects.equals(this.dateOfBirth, user.dateOfBirth) &&
         Objects.equals(this.email, user.email) &&
         Objects.equals(this.password, user.password) &&
-        Objects.equals(this.userType, user.userType) &&
+        Objects.equals(this.userTypeEnum, user.userTypeEnum) &&
         Objects.equals(this.dayLimit, user.dayLimit) &&
         Objects.equals(this.transactionLimit, user.transactionLimit) &&
         Objects.equals(this.open, user.open);
@@ -316,7 +341,7 @@ public class User   {
 
   @Override
   public int hashCode() {
-    return Objects.hash(id, firstName, lastName, dateOfBirth, email, password, userType, dayLimit, transactionLimit, open);
+    return Objects.hash(id, firstName, lastName, dateOfBirth, email, password, userTypeEnum, dayLimit, transactionLimit, open);
   }
 
   @Override
@@ -330,7 +355,7 @@ public class User   {
     sb.append("    dateOfBirth: ").append(toIndentedString(dateOfBirth)).append("\n");
     sb.append("    email: ").append(toIndentedString(email)).append("\n");
     sb.append("    password: ").append(toIndentedString(password)).append("\n");
-    sb.append("    userType: ").append(toIndentedString(userType)).append("\n");
+    sb.append("    userType: ").append(toIndentedString(userTypeEnum)).append("\n");
     sb.append("    dayLimit: ").append(toIndentedString(dayLimit)).append("\n");
     sb.append("    transactionLimit: ").append(toIndentedString(transactionLimit)).append("\n");
     sb.append("    open: ").append(toIndentedString(open)).append("\n");
@@ -342,7 +367,7 @@ public class User   {
    * Convert the given object to string with each line indented by 4 spaces
    * (except the first line).
    */
-  private String toIndentedString(java.lang.Object o) {
+  private String toIndentedString(Object o) {
     if (o == null) {
       return "null";
     }
