8026e54c995f1383c10101c7b98ee817aadeb95c
LionelSumter <50021812+LionelSumter@users.noreply.github.com>
2021-05-25 18:17:26 +0200

    Add files via upload
    Testdata in MyApplicationRunner gezet

--- src/main/java/io/swagger/configuration/MyApplicationRunner.java
+++ src/main/java/io/swagger/configuration/MyApplicationRunner.java
@@ -1,16 +1,31 @@
 package io.swagger.configuration;
 
+import io.swagger.model.User;
+import io.swagger.service.UserService;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+import org.threeten.bp.LocalDate;
+
+import java.math.BigDecimal;
 
 @Component
 public class MyApplicationRunner implements ApplicationRunner {
 
-    //Add all repositories here and in constructor (or @Autowired)
+
+    private UserService userService;
+
+    public MyApplicationRunner(UserService userService) {
+        this.userService = userService;
+    }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        // Create one new user
+        User user1 = new User(1L, "test", "test", LocalDate.of(2021,12,20), "lio","password", User.UserTypeEnum.EMPLOYEE, new BigDecimal("100.02"), new BigDecimal("200.02"), false);
+
+        // Add the shops
+        userService.createUser(user1);
 
     }
 }
