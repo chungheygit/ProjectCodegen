91cf2858fa3dacca2661af059124a67ca86ac9a8
salahb10 <47417425+salahb10@users.noreply.github.com>
2021-05-29 13:07:42 +0200

    create & update account werkt

--- src/main/java/io/swagger/repository/AccountRepository.java
+++ src/main/java/io/swagger/repository/AccountRepository.java
@@ -4,6 +4,7 @@ import io.swagger.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.threeten.bp.LocalDate;
 
 import java.util.Optional;
 
@@ -12,5 +13,8 @@ public interface AccountRepository extends JpaRepository<Account,Long> {
 
     @Query("select a from Account a where a.iban = ?1")
     public Account getAccountByIban(String iban);
+
+    @Query("select a from Account a where a.createdDate =:createdDate  ")
+    public Account getAccountByCreatedDate(LocalDate createdDate);
 }
 
