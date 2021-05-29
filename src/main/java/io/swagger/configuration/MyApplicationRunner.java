d4a796cf6403775fa717039c9449585277521fd7
chungheylam <chungheyg@gmail.com>
2021-05-29 13:43:28 +0200

    testdata

--- src/main/java/io/swagger/configuration/MyApplicationRunner.java
+++ src/main/java/io/swagger/configuration/MyApplicationRunner.java
@@ -36,14 +36,18 @@ public class MyApplicationRunner implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         // Create one new user
         User user1 = new User(1L, "test", "test", LocalDate.of(2021,12,20), "lio","password", User.UserTypeEnum.EMPLOYEE, new BigDecimal("100.02"), new BigDecimal("200.02"), false);
+        User user2 = new User(2L, "test", "test", LocalDate.of(2021,12,20), "lio","password", User.UserTypeEnum.EMPLOYEE, new BigDecimal("100.02"), new BigDecimal("200.02"), false);
 
         // Add the shops
         userService.createUser(user1);
+        userService.createUser(user2);
 
         // Create an Account
-        Account account1 = new Account(1L, "NL58INHO0123456789", new BigDecimal(9999.25 ), LocalDate.of(2021,05,27), Account.AccountTypeEnum.CURRENT, new BigDecimal(500 ), true);
+        Account account1 = new Account(1L, "NL58INHO0123456789", new BigDecimal(9999.25 ), LocalDate.of(2021,05,27), Account.AccountTypeEnum.SAVINGS, new BigDecimal(500 ), true);
+        Account account2 = new Account(2L, "NL58INHO0123456788", new BigDecimal(9999.25 ), LocalDate.of(2021,05,27), Account.AccountTypeEnum.CURRENT, new BigDecimal(500 ), true);
         // Add account
 
         accountService.addAccount(account1);
+        accountService.addAccount(account2) ;
     }
 }
