7c64711b72dcc54302650bd21536314b0739d707
LionelSumter <50021812+LionelSumter@users.noreply.github.com>
2021-05-30 20:22:08 +0200

    Add files via upload
    Fix import java.time.LocalDate

--- src/main/java/io/swagger/configuration/MyApplicationRunner.java
+++ src/main/java/io/swagger/configuration/MyApplicationRunner.java
@@ -5,9 +5,10 @@ import io.swagger.service.UserService;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-import org.threeten.bp.LocalDate;
+//import org.threeten.bp.LocalDate;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Component
 public class MyApplicationRunner implements ApplicationRunner {
@@ -22,7 +23,7 @@ public class MyApplicationRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         // Create one new user
-        User user1 = new User(1L, "test", "test", LocalDate.of(2021,12,20), "lio","password", User.UserTypeEnum.EMPLOYEE, new BigDecimal("100.02"), new BigDecimal("200.02"), false);
+        User user1 = new User(1L, "test", "test", LocalDate.of(2017, 1, 13), "lio","password", User.UserTypeEnum.EMPLOYEE, new BigDecimal("100.02"), new BigDecimal("200.02"), false);
 
         // Add the shops
         userService.createUser(user1);
