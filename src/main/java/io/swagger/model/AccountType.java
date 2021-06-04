1013372b8d51d565bb79640c3dfde3bd1e0bafe5
Yassine640514 <47414054+Yassine640514@users.noreply.github.com>
2021-06-04 13:03:53 +0200

    improved code in Transaction and User classes

---
+++ src/main/java/io/swagger/model/AccountType.java
@@ -0,0 +1,35 @@
+package io.swagger.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ * Gets or Sets accountType
+ */
+public enum AccountType {
+    CURRENT("current"),
+
+    SAVINGS("savings");
+
+    private String value;
+
+    AccountType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    @JsonValue
+    public String toString() {
+        return String.valueOf(value);
+    }
+
+    @JsonCreator
+    public static AccountType fromValue(String text) {
+        for (AccountType b : AccountType.values()) {
+            if (String.valueOf(b.value).equals(text)) {
+                return b;
+            }
+        }
+        return null;
+    }
+}
