623642c269d36acca5ee7b6de3b20e8b8d9a3d11
LionelSumter <50021812+LionelSumter@users.noreply.github.com>
2021-06-05 14:48:56 +0200

    Add files via upload
    Merge

--- src/main/java/io/swagger/repository/AccountRepository.java
+++ src/main/java/io/swagger/repository/AccountRepository.java
@@ -2,13 +2,22 @@ package io.swagger.repository;
 
 import io.swagger.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.threeten.bp.LocalDate;
 
 import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account,Long> {
 
+    @Query("select a from Account a where a.iban = ?1")
+    public Account getAccountByIban(String iban);
+
+    @Query("select a from Account a where a.createdDate =:createdDate  ")
+    public Account getAccountByCreatedDate(LocalDate createdDate);
 
 }
 
