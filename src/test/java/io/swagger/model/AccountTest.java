327c7d7fa773efcba38cf3ee8b2efa19a7bb7b89
salahb10 <47417425+salahb10@users.noreply.github.com>
2021-06-06 18:25:10 +0200

    AccountTest werkt

---
+++ src/test/java/io/swagger/model/AccountTest.java
@@ -0,0 +1,62 @@
+package io.swagger.model;
+
+import io.swagger.service.AccountService;
+import io.swagger.service.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class AccountTest {
+
+    private Account account;
+    private AccountService accountService;
+    private UserService userService;
+
+    @BeforeEach
+    public void init(){
+        account = new Account(1L,"NL58INHO0123456789", new BigDecimal(9999.25 ), java.time.LocalDate.of(2021,05,27), AccountType.CURRENT, new BigDecimal(500 ), true );
+        accountService = new AccountService();
+    }
+    @Test
+    public void creatingAccountShouldNotBeNull(){
+        Assertions.assertNotNull(account);
+    }
+    @Test
+    public void getAccountTypeCurrentReturnAccountCurrent() {
+        assertEquals(AccountType.CURRENT, account.getAccountType());
+    }
+    @Test
+    public void setAccountTypeShouldUpdateAccountType() {
+        account.setAccountType(AccountType.SAVINGS);
+        assertEquals(AccountType.SAVINGS, account.getAccountType());
+    }
+    @Test
+    public void getIbanShouldReturnIban() {
+        assertEquals("NL58INHO0123456789", account.getIban());
+    }
+
+    @Test
+    public void setIbanShouldUpdateIban() {
+        account.setIban("NL58INHO0123456789");
+        assertEquals("NL58INHO0123456789", account.getIban());
+    }
+
+    @Test
+    public void setAbsoluteLimitShouldUpdateAbsoluteLimit() {
+        account.setAbsoluteLimit(BigDecimal.valueOf(200));
+        assertEquals(BigDecimal.valueOf(200), account.getAbsoluteLimit());
+    }
+
+    @Test
+    public void getUserIdShouldReturnUserId() {
+        assertEquals(1L, account.getUserId());
+    }
+
+}
--- src/test/java/io/swagger/service/AccountServiceTest.java
+++ src/test/java/io/swagger/service/AccountServiceTest.java
@@ -1,7 +1,28 @@
 package io.swagger.service;
 
+import io.swagger.model.Account;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class AccountServiceTest {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private AccountService accountService;
+
+
+
+    @Test
+    public void testGetAllAccount() throws Exception {
+        List<Account> accountResponses = accountService.GetAllAccounts();
+        assertTrue(accountResponses.size() > 0);
+    }
+
 
 }
