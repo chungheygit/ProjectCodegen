b4dd2e4b793e526d0d6daac098db6e413ae72447
chungheylam <chungheyg@gmail.com>
2021-06-06 13:12:12 +0200

    amount check

---
+++ src/test/java/io/swagger/api/controller/TransactionsApiControllerTest.java
@@ -0,0 +1,18 @@
+package io.swagger.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TransactionsApiControllerTest {
+
+@Autowired
+private MockMvc mockMvc;
+
+
+}
