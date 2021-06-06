856806b7ba1fd233672f83f4bef90dfbab06d7aa
Yassine640514 <47414054+Yassine640514@users.noreply.github.com>
2021-06-06 11:03:46 +0200

    fixed lost code

--- src/main/java/io/swagger/api/UsersApiController.java
+++ src/main/java/io/swagger/api/UsersApiController.java
@@ -1,6 +1,6 @@
 package io.swagger.api;
 
-import io.swagger.model.Login;
+import io.swagger.model.DTO.LoginDTO;
 import io.swagger.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.service.UserService;
@@ -11,13 +11,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.*;
 import javax.validation.Valid;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,6 +41,8 @@ public class UsersApiController implements UsersApi {
         this.userService = userService;
     }
 
+    // CREATE A NEW USER
+    @PreAuthorize("hasRole('Employee')")
     public ResponseEntity<User> createUser(@Parameter(in = ParameterIn.DEFAULT, description = "", required=true, schema=@Schema()) @Valid @RequestBody User body) {
         String accept = request.getHeader("Accept");
         //Create User
@@ -50,30 +53,31 @@ public class UsersApiController implements UsersApi {
         }
     }
 
+    // GET ALL USERS
     public ResponseEntity<List<User>> getAllUsers(@Min(0)@Parameter(in = ParameterIn.QUERY, description = "The number of items to skip before starting to \\ collect the result set" ,schema=@Schema(allowableValues={  }
 )) @Valid @RequestParam(value = "offset", required = false) Integer offset,@Min(0)@Parameter(in = ParameterIn.QUERY, description = "The numbers of items to return" ,schema=@Schema(allowableValues={  }
-)) @Valid @RequestParam(value = "limit", required = false) Integer limit,@Parameter(in = ParameterIn.QUERY, description = "filter users by email" ,schema=@Schema()) @Valid @RequestParam(value = "email", required = false) String email) {
+)) @Valid @RequestParam(value = "limit", required = false) Integer limit,@Parameter(in = ParameterIn.QUERY, description = "filter users by email" ,schema=@Schema()) @Valid @RequestParam(value = "email", required = false) String email) throws Exception {
         String accept = request.getHeader("Accept");
 
-        //Get all users
+        List<User> users = new ArrayList<User>();
         try {
-            //filter email
+            // GET ALL USERS BY MAIL
             if (email != null) {
                 User u = userService.findUserByEmail(email);
-                List<User> users = new ArrayList<User>();
+                //users = new ArrayList<User>();
                 if (u == null)
                     return new ResponseEntity(HttpStatus.NOT_ACCEPTABLE);
                 users.add(u);
                 return new ResponseEntity<List<User>>(users, HttpStatus.OK);
             }
-            return new ResponseEntity<List<User>>(userService.getAllUsers(),HttpStatus.OK);
+            //RETURN USERS WITH OR WITHOUT LIMIT AND OFFSET
+            return new ResponseEntity<List<User>>(userService.getAllUsers(limit, offset), HttpStatus.OK);
         } catch (IllegalArgumentException iae) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
-
-
     }
 
+    // GET A USER BY ITS ID
     public ResponseEntity<User> getUserById(@Min(0)@Parameter(in = ParameterIn.PATH, description = "Id of a user", required=true, schema=@Schema(allowableValues={  }
 )) @PathVariable("userId") Integer userId) {
         String accept = request.getHeader("Accept");
@@ -85,33 +89,34 @@ public class UsersApiController implements UsersApi {
         }
     }
 
-    public ResponseEntity<String> loginUser(@Parameter(in = ParameterIn.DEFAULT, description = "", required=true, schema=@Schema()) @Valid @RequestBody Login body) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<String>(objectMapper.readValue("\"\"", String.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
+    public ResponseEntity<String> loginUser(@Parameter(in = ParameterIn.DEFAULT, description = "", required=true, schema=@Schema()) @Valid @RequestBody LoginDTO body) {
+        try {
+            return new ResponseEntity<String>(userService.login(body), HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Couldn't serialize response for content type application/json", e);
+            return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-
-        return new ResponseEntity<String>(HttpStatus.NOT_IMPLEMENTED);
     }
 
     public ResponseEntity<User> updateUser(@Min(0)@Parameter(in = ParameterIn.PATH, description = "Id of a user", required=true, schema=@Schema(allowableValues={  }
-)) @PathVariable("userId") Integer userId) {
+)) @PathVariable("userId") Integer userId, @Valid @RequestBody User body) {
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
             try {
-                return new ResponseEntity<User>(objectMapper.readValue("{\n  \"firstName\" : \"firstName\",\n  \"lastName\" : \"lastName\",\n  \"password\" : \"password\",\n  \"dayLimit\" : 1.6027456183070403,\n  \"dateOfBirth\" : \"2000-01-23\",\n  \"id\" : 1,\n  \"userType\" : [ \"customer\", \"customer\" ],\n  \"transactionLimit\" : 1.1465812980502945,\n  \"email\" : \"email\",\n  \"open\" : false\n}", User.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
+                    User targetUser = userService.getUserById(userId);
+
+            } catch (Exception e) {
                 log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<User>(HttpStatus.INTERNAL_SERVER_ERROR);
+                return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
             }
+            if(!userService.IsLoggedInUserEmployee()){
+                return new ResponseEntity<User>(HttpStatus.UNAUTHORIZED);
+            }
+            body.setId(Long.valueOf(userId.longValue()));
+            return new ResponseEntity<User>(userService.updateUser(body), HttpStatus.OK);
         }
 
-        return new ResponseEntity<User>(HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
     }
 
 }
