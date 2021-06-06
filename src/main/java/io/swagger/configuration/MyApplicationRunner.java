9b404c8a8e3a4b7329cb60a802fe15ebaff6bf79
LionelSumter <50021812+LionelSumter@users.noreply.github.com>
2021-06-06 15:47:37 +0200

    Update MyApplicationRunner.java
    Fix time

--- src/main/java/io/swagger/configuration/MyApplicationRunner.java
+++ src/main/java/io/swagger/configuration/MyApplicationRunner.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-import org.threeten.bp.LocalDate;
+import java.time.LocalDate;
 import org.threeten.bp.OffsetDateTime;
 
 import java.math.BigDecimal;
