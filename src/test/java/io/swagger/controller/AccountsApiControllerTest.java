bb9c87c357dadfa7ab458486bb00019636fecda6
Yassine640514 <47414054+Yassine640514@users.noreply.github.com>
2021-06-06 23:09:02 +0200

    final touches

--- src/test/java/io/swagger/controller/AccountsApiControllerTest.java
+++ src/test/java/io/swagger/controller/AccountsApiControllerTest.java
@@ -1,20 +1,12 @@
 package io.swagger.controller;
 
-import io.swagger.model.Account;
-import io.swagger.model.AccountType;
-import io.swagger.service.AccountService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
@@ -26,20 +18,10 @@ public class AccountsApiControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @MockBean
-    private AccountService service;
-    private Account account;
-    private List<Account> accounts;
+    @Test
+    @DisplayName("Test if a negative balance limit can be a positive number")
+    public void testIfNegativeBalanceLimit() throws Exception {
 
-    public void init(){
 
-        accounts =
-                Arrays.asList(
-                        new Account(1L, "NL58INHO0123456789", new BigDecimal(9999.25 ), java.time.LocalDate.of(2021,05,27), AccountType.CURRENT, new BigDecimal(500 ), true),
-                        new Account(2L, "NL58INHO0123456788", new BigDecimal(200 ), java.time.LocalDate.of(2021,05,27), AccountType.CURRENT, new BigDecimal(500 ), true),
-                        new Account(3L, "NL58INHO0123456701", new BigDecimal(9999.25 ), java.time.LocalDate.of(2021,05,27), AccountType.CURRENT, new BigDecimal(500 ), true),
-                        new Account(4L, "NL58INHO0123456702", new BigDecimal(9999.25 ), java.time.LocalDate.of(2021,05,27), AccountType.CURRENT, new BigDecimal(500 ), false)
-                );
     }
-
 }
