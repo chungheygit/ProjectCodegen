c690a5424963c4c2f0f0ce9ffde5185ab4f98eeb
salahb10 <47417425+salahb10@users.noreply.github.com>
2021-06-16 13:53:35 +0200

    feedback account klaar + preathorize

---
+++ src/main/java/io/swagger/model/DTO/UpdateAccountDTO.java
@@ -0,0 +1,55 @@
+package io.swagger.model.DTO;
+
+import io.swagger.model.AccountType;
+
+import java.math.BigDecimal;
+
+public class UpdateAccountDTO {
+    private Boolean open;
+    private BigDecimal absoluteLimit;
+    private BigDecimal balance;
+    private AccountType accountType;
+
+    public UpdateAccountDTO(Boolean open, BigDecimal absoluteLimit, BigDecimal balance, AccountType accountType) {
+
+        this.open = open;
+        this.absoluteLimit = absoluteLimit;
+        this.balance = balance;
+        this.accountType = accountType;
+    }
+
+    public UpdateAccountDTO() {
+    }
+
+    public Boolean getOpen() {
+        return open;
+    }
+
+    public void setOpen(Boolean open) {
+        this.open = open;
+    }
+
+    public BigDecimal getAbsoluteLimit() {
+        return absoluteLimit;
+    }
+
+    public void setAbsoluteLimit(BigDecimal absoluteLimit) {
+        this.absoluteLimit = absoluteLimit;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(AccountType accountType) {
+        this.accountType = accountType;
+    }
+}
