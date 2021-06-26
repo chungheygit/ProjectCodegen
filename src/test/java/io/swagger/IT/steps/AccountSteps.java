1524e135b808005a3559ffcd471542d90bd1efc1
salahb10 <47417425+salahb10@users.noreply.github.com>
2021-06-26 20:35:20 +0200

    Accounts af

--- src/test/java/io/swagger/IT/steps/AccountSteps.java
+++ src/test/java/io/swagger/IT/steps/AccountSteps.java
@@ -22,6 +22,7 @@ import org.springframework.http.*;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.threeten.bp.OffsetDateTime;
 
 import java.math.BigDecimal;
 import java.net.URI;
@@ -40,13 +41,10 @@ public class AccountSteps {
     private ObjectMapper mapper = new ObjectMapper();
     User user = new User("N'Golo", "Kanté",LocalDate.of(2021,3,18), "emp","password", UserType.ROLE_Employee, new BigDecimal("1000.02"), new BigDecimal("250.02"), false);
     Account account =   new Account(user.getId(), "NL58INHO0123456702", new BigDecimal(7999.25 ), java.time.LocalDate.of(2021,3,18), AccountType.CURRENT, new BigDecimal(500 ), false);
-//    # voor het runnen van deze testen moet er eerst code gecomment worden.REGEL 87
-//    # voor het runnen van deze testen moet er eerst code gecomment worden.REGEL 87
-//    # voor het runnen van deze testen moet er eerst code gecomment worden.REGEL 87
-//    # voor het runnen van deze testen moet er eerst code gecomment worden.REGEL 87
 
 
-    @When("Employee log in")
+
+    @When("Employee login")
     public void ILogInWithCorrectCredentials() throws URISyntaxException, JsonProcessingException {
         LoginDTO loginDTO = new LoginDTO();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -55,9 +53,9 @@ public class AccountSteps {
         responseEntity = template.postForEntity(uri, entity, String.class);
     }
 
-    @When("Ik alle accounts ophaal")
+    @When("I get all accounts")
     @WithMockUser(username = "emp", password = "password", roles = "Employee")
-    public void ikAlleAccountsOphaal() throws URISyntaxException {
+    public void IGetAllAccounts() throws URISyntaxException {
         URI uri = new URI(baseURL + "accounts/");
         headers.setBearerAuth(responseEntity.getBody());
         HttpEntity<String> entity = new HttpEntity<String>(null, headers);
@@ -65,67 +63,46 @@ public class AccountSteps {
     };
 
 
-    @Then("Is de status van het request {int}")
-    public void isDeStatusVanHetRequest(int expected) {
+    @Then("Is the status of the request {int}")
+    public void isTheStatusOfTheRequest(int expected) {
         int response = responseEntity.getStatusCodeValue();
         Assert.assertEquals(expected, response);
     }
 
-    @Then("Krijg een lijst terug van accounts")
-    public void krijgEenLijstTerugVanAccounts() throws JSONException {
+    @Then("Get a list of accounts back")
+    public void GetAListOfAccountsBack() throws JSONException {
         String response = responseEntity.getBody();
         JSONArray jsonArray = new JSONArray(response);
-        Assert.assertEquals(2, jsonArray.length());
+        Assert.assertEquals(1, jsonArray.length());
 
     }
 
-    @When("Ik een Account ophaal met iban {string}")
-    public void ikEenAccountOphaalMetIban(String iban) throws URISyntaxException {
+    @When("I get an account with iban {string}")
+    public void IGetAccountWithIban(String iban) throws URISyntaxException {
         URI uri = new URI(baseURL + "accounts/" + iban);
         headers.setBearerAuth(responseEntity.getBody());
         HttpEntity<String> entity = new HttpEntity<String>(null, headers);
         //responseEntity = template.exchange(uri, HttpMethod.GET, entity, String.class);
     }
 
-    @Then("Is het account absoluteLimit {double}")
-    public void isHetAccountAbsoluteLimit(double absoluteLimit) throws JSONException {
-        String response = responseEntity.getBody();
-        JSONObject jsonObject = new JSONObject(response);
-        Assert.assertEquals(absoluteLimit, jsonObject.getDouble("absoluteLimit"), 0);
-    }
 
-    @When("Ik een Account ophaal met Username {string}")
-    public void ikEenAccountOphaalMetUsername(String username) throws URISyntaxException {
-        URI uri = new URI(baseURL + "accounts/" + "username/" + username);
-        headers.setBearerAuth(responseEntity.getBody());
-        HttpEntity<String> entity = new HttpEntity<>(null, headers);
-        responseEntity = template.exchange(uri, HttpMethod.GET, entity, String.class);
-
-    }
-
-    @Then("Is het iban van het account {string}")
-    public void isHetIbanVanHetAccount(String iban) throws JSONException {
+    @Then("Get account with iban back {string}")
+    public void GetAccountWithIbanBack(String iban) throws JSONException {
         String response = responseEntity.getBody();
         JSONArray jsonArray = new JSONArray(response);
-        Assert.assertEquals(iban, jsonArray.getJSONObject(0).getString("iban"));
+        Assert.assertEquals(iban, jsonArray.length());
+
+
     }
-
-
-    @Then("Ik een Account maak met AbsoluteLimit {double} en owner {string} en type {string} en active Status.Active")
-    public void ikEenAccountMaakMetAbsoluteLimitEnOwnerEnTypeEnActiveStatusActive(double absoluteLimit, String owner, String type) throws URISyntaxException, JsonProcessingException {
-
-        mapper = new ObjectMapper();
-        URI uri = new URI(baseURL + "accounts");
-        headers.setBearerAuth(responseEntity.getBody());
+    @When("I post an account")
+    public void iPostAnAccount() throws JsonProcessingException, URISyntaxException {
+        ObjectMapper mapper = new ObjectMapper();
+        Account account= new Account(1L,"NL58INHO0123456799",new BigDecimal(1000),LocalDate.now(),AccountType.CURRENT,new BigDecimal(500),true );
+        URI uri = new URI(baseURL);
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> entity = new HttpEntity<>(mapper.writeValueAsString(account), headers);
-        responseEntity = template.exchange(uri, HttpMethod.POST, entity, String.class);
-
+        responseEntity = template.postForEntity(uri, entity, String.class);
     }
-
-
-
-
 }
 
 
--- src/test/java/io/swagger/service/UserServiceTest.java
+++
@@ -1,7 +0,0 @@
-package io.swagger.service;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class UserServiceTest {
-
-}
