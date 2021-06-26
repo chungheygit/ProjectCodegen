3014e935db385e0ef3d1ab801654fbb9e5cc7cca
LionelSumter <50021812+LionelSumter@users.noreply.github.com>
2021-06-26 23:50:34 +0200

    Add userControllerTest

--- src/test/java/io/swagger/controller/UsersApiControllerTest.java
+++ src/test/java/io/swagger/controller/UsersApiControllerTest.java
@@ -1,38 +1,18 @@
 package io.swagger.controller;
 
-import io.swagger.api.UsersApi;
-import io.swagger.api.UsersApiController;
-import io.swagger.model.Account;
-import io.swagger.model.DTO.AccountDTO;
-import io.swagger.model.DTO.UpdateAccountDTO;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.model.User;
 import io.swagger.model.UserType;
-import io.swagger.service.AccountService;
 import io.swagger.service.UserService;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.security.authentication.BadCredentialsException;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -40,9 +20,10 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -50,71 +31,87 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class UsersApiControllerTest {
 
     @Autowired
-    private UsersApi api;
     private MockMvc mvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @MockBean
     private UserService userService;
+    private List<User> users;
     private User user;
-    private User user1;
+    private User userWithSameEmail;
+    private User userEmailWithoutConstraints;
 
     @BeforeEach
-    public void setUp() {
-        //Spring boot login
-        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken("emp","password"));
-    }
+    public void setup() {
+        users =
+                Arrays.asList(
+                        new User("Bruno", "Fernandes", LocalDate.of(2021,1,25), "mark@inholland.nl","password", UserType.ROLE_Employee, new BigDecimal("1000.02"), new BigDecimal("250.02"), true),
+                        new User("Frenkie", "De Jong", LocalDate.of(2021,4,20), "lio@test2.com","password", UserType.ROLE_Customer, new BigDecimal("1000.02"), new BigDecimal("250.02"), true),
+                        new User("Kevin", "De Bruyne", LocalDate.of(2021,6,1), "cus","password", UserType.ROLE_Customer, new BigDecimal("1000.02"), new BigDecimal("250.02"), false),
+                        new User("N'Golo", "Kanté", LocalDate.of(2021,3,18), "emp","password", UserType.ROLE_Employee, new BigDecimal("1000.02"), new BigDecimal("250.02"), true),
+                        new User("Bank", "account", LocalDate.of(1999,9,19), "bank@bankgroep6.com","groep6bank", UserType.ROLE_Employee, new BigDecimal("1000000.00"), new BigDecimal("100000.00"), true)
+                );
 
-    @Test
-    //GetUserByID
-    public void getUserByIdTest() throws Exception {
-        Integer id = 4;
-        ResponseEntity<User>responseEntity = api.getUserById(id);
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-    }
-
-    //CreateUser
-    @Test
-    public void createAUserWithPasswordReturns201Created() throws Exception {
-        User body = new User();
-        body.setPassword("test");
-        //body.setLastName("test");
-        ResponseEntity<User> responseEntity = api.createUser(body);
-        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        user = new User("N'Golo", "Kanté", LocalDate.of(2021,3,18), "Kees@hotmail.com","keesPassword", UserType.ROLE_Employee, new BigDecimal("1000.02"), new BigDecimal("250.02"), true);
+        userWithSameEmail =  new User("N'Golo", "Kanté", LocalDate.of(2021,3,18), "emp","password", UserType.ROLE_Employee, new BigDecimal("1000.02"), new BigDecimal("250.02"), true);
+        userEmailWithoutConstraints = new User("User", "user", LocalDate.of(2021,3,18), "withoutConstraints","password", UserType.ROLE_Employee, new BigDecimal("1000.02"), new BigDecimal("250.02"), true);
     }
 
 
     @Test
-    //GetUserByExistingEmail
-    public void createAUserWithExistingEmailReturnsException() throws Exception {
-        User body = new User();
-        body.setFirstName("hans");
-        body.setEmail("cus");
-        body.setId(100L);
-        body.setPassword("password");
-        body.setDayLimit( new BigDecimal(85000000.00 ));
-        body.setDateOfBirth(LocalDate.of(2021,1,25));
-        body.setOpen(false);
-        body.setTransactionLimit( new BigDecimal(85000000.00 ));
-        body.setUserType(UserType.ROLE_Employee);
-        ResponseEntity<User> responseEntity = api.createUser(body);
-        assertEquals(HttpStatus.FORBIDDEN, responseEntity.getStatusCode());
-    }
-
-    @Test
-    //GetAllUsers
-    public void getUsersTest() throws Exception {
-        Integer limit = 12;
-        Integer offset = null;
-        String email = "empp";
-        ResponseEntity<List<User>> responseEntity = api.getAllUsers(offset, limit, email);
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-    }
-
-    @Test
-    @WithMockUser(username = "emp", password = "password", roles = "Employee")
-    public void updateAccountShouldReturnOk() throws Exception {
-        given(userService.updateUser(user1)).willReturn(user);
+    @WithMockUser(username="emp",roles={"Employee"})
+    public void callingAllUsersWithoutFiltersShouldReturnOk() throws Exception {
+        given(userService.getAllUsers(0, 0)).willReturn(users);
         this.mvc.perform(get("/users/")).andExpect(
+                status().isOk()).andExpect(jsonPath("$", hasSize(0)));
+    }
+
+    @Test
+    @WithMockUser(username="emp",roles={"Employee"})
+    public void getUser4ShouldReturnOk() throws Exception {
+        given(userService.getUserById(4L)).willReturn(user);
+        this.mvc.perform(get("/users/4/")).andExpect(
                 status().isOk());
     }
+
+    @Test
+    public void getUser4WithoutMockUserShouldReturnForbidden() throws Exception {
+        given(userService.getUserById(4L)).willReturn(user);
+        this.mvc.perform(get("/users/4/")).andExpect(
+                status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username="emp",roles={"Employee"})
+    public void postingUserShouldReturn201Created() throws Exception {
+        this.mvc.perform(
+                post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    @WithMockUser(username="emp",roles={"Employee"})
+    public void postingUserWithSameEmailShouldReturn403Forbidden() throws Exception {
+        this.mvc.perform(
+                post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userWithSameEmail)))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @WithMockUser(username="emp",roles={"Employee"})
+    public void postingUserWithEmailWithoutConstraintsShouldReturn403Forbidden() throws Exception {
+        this.mvc.perform(
+                post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userEmailWithoutConstraints)))
+                .andExpect(status().isForbidden());
+    }
+
+
 }
